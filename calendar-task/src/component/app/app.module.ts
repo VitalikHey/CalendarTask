fe5e09682a713e76b5638211d4f146169584420d
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-06-13 10:29:42 +0300

    Refactor name

--- calendar-task/src/component/app/app.module.ts
+++ calendar-task/src/component/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskCalendarComponent } from '../task-calendar/task-calendar.component';
-import { JulainPipe } from '../../pipes/calendar-pipe/calendar.pipe';
+import { JulainPipe } from '../../pipes/calendar-pipe/julian.pipe';
 import { FormsModule } from '@angular/forms';
 import { BaikalPipe } from '../../pipes/baikal-pipe/baikal.pipe';
 
