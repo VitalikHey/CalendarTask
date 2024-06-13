47a1191adb65043650326efbb0b3fb2d44063e5a
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-06-13 10:59:56 +0300

    Refactor the literal template

--- calendar-task/src/pipes/generals-functions.ts
+++ calendar-task/src/pipes/generals-functions.ts
@@ -2,10 +2,10 @@ export function changeCalendar(value: Date, countDay: number): string {
   const newDate: Date = new Date(value);
   const currentDay: number = value.getDate();
   newDate.setDate(currentDay + countDay);
-  return `${'Дѣнь ' +
-    newDate.getDate() +
+  return 'Дѣнь ' +
+    `${newDate.getDate()}` +
     ' Мѣсяцъ ' +
-    (newDate.getMonth() + 1) +
+    `${(newDate.getMonth() + 1)}` +
     ' Годъ ' +
-    newDate.getFullYear()}`
+    `${newDate.getFullYear()}`
 }
