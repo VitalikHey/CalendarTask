27808ea49940be25f7d172d0e4acd1ed572b8088
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-06-13 11:04:37 +0300

    Refactor pipes

--- calendar-task/src/pipes/baikal-pipe/baikal.pipe.ts
+++ calendar-task/src/pipes/baikal-pipe/baikal.pipe.ts
@@ -1,13 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { changeCalendar } from '../generals-functions';
 
-const needCountDay: number = 2
-
 @Pipe({
   name: 'baikal',
 })
 export class BaikalPipe implements PipeTransform {
   transform(value: Date): string {
-    return changeCalendar(value, needCountDay);
+    return changeCalendar(value, 2);
   }
 }
--- calendar-task/src/pipes/calendar-pipe/julian.pipe.ts
+++ calendar-task/src/pipes/calendar-pipe/julian.pipe.ts
@@ -1,13 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { changeCalendar } from '../generals-functions';
 
-const needCountDay: number = 14
-
 @Pipe({
   name: 'julian',
 })
 export class JulianPipe implements PipeTransform {
   transform(value: Date): string {
-    return changeCalendar(value, needCountDay);
+    return changeCalendar(value, 14);
   }
 }
